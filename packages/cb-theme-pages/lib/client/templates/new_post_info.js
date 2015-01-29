f4fb563aaf1e507bebd2a73634d99a0a5a57951f
lpatmo <linda.peng@alumni.duke.edu>
2015-01-29 00:00:49 -0500

    trying to fix canEdit

--- packages/cb-theme-pages/lib/client/templates/new_post_info.js
+++ packages/cb-theme-pages/lib/client/templates/new_post_info.js
@@ -2,9 +2,6 @@ Template[getTemplate('newPostInfo')].helpers({
   pointsUnitDisplayText: function(){
     return this.upvotes == 1 ? i18n.t('participant') : i18n.t('participants');
   },
-  can_edit: function(){
-    return canEdit(Meteor.user(), this);
-  },
   getTemplate: function() {
     return getTemplate("newPostAuthor");
   }
