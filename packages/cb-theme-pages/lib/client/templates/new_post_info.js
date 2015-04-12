4acc8ea2b66fe10cf34da24f0d772668612ae042
lpatmo <linda.peng@alumni.duke.edu>
2015-04-12 04:11:40 -0400

    a couple of important changes - subscribers to participants, added footnotes

--- packages/cb-theme-pages/lib/client/templates/new_post_info.js
+++ packages/cb-theme-pages/lib/client/templates/new_post_info.js
@@ -1,6 +1,6 @@
 Template[getTemplate('newPostInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('participant') : i18n.t('participants');
+    return this.upvotes == 1 ? i18n.t('subscriber') : i18n.t('subscribers');
   },
   getTemplate: function() {
     return getTemplate("newPostAuthor");
