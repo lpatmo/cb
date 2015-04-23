2b6644598be08f0beb3a828daf19cd7e951c22b9
lpatmo <linda.peng@alumni.duke.edu>
2015-04-23 02:24:57 -0400

    singular person

--- packages/cb-theme-pages/lib/client/templates/new_post_info.js
+++ packages/cb-theme-pages/lib/client/templates/new_post_info.js
@@ -2,7 +2,7 @@ Meteor.startup(function () {
 
 Template[getTemplate('newPostInfo')].helpers({
   pointsUnitDisplayText: function(){
-    return this.upvotes == 1 ? i18n.t('people interested') : i18n.t('people interested');
+    return this.upvotes == 1 ? i18n.t('person interested') : i18n.t('people interested');
   },
   getTemplate: function() {
     return getTemplate("newPostAuthor");
